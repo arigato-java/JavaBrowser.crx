207f05e9b23d88d12339e062657f3c3cdf5f6c4d
tobynet <toby.netinfo+git@gmail.com>
2017-03-06 17:44:38 +0900

    Fix a performance issue. And refactor a little.
    Thank you the ArigatoJava operation team, for suggesting  changes to other API !

--- newtab.js
+++ newtab.js
@@ -3,60 +3,91 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+/**
+ *  表示時にジャバ格言をセット
+ */
 function updateKakugen() {
-  // 表示時にジャバ格言をセット
-  //
-  // ref. ありジャバAPI https://arigato-java.download/api.html
-  //
-  // ex.
-  // 
-  //  > curl -H "Content-Type: application/json" -X POST -d '{"topic":"0"}' \
-  //    https://ue4higkk44.execute-api.us-west-2.amazonaws.com/prod/kakugen
-  //  
-  const API_URI = "https://ue4higkk44.execute-api.us-west-2.amazonaws.com/prod/kakugen";
+  const API_URI = "https://arigato-java.download/kakugen.json";
   
-  fetch(API_URI, {
-    method: "POST",
-    body: '{"topic":"0"}'
+  // ブラウザのキャッシュを期待して、実直に GET しにいく
+  fetch(API_URI)
+  .then(extractJSON)
+  .then(pickRandomly)
+  .catch(err => {
+    console.error("API Error: ", err);
+    return {
+      t: "ジャバ.lang.NullPointerException\n" + 
+         "at ジャバ人間ブラウザ.main(...)" 
+    }
   })
-  .then(res => res.json())
-  .then(j => {
-    console.log(j);
-
-    // 格言内容を表示
-    const txt = document.getElementById("kakugen-txt");
-    if (j.kakugen) {
-      txt.textContent = j.kakugen;
-    } else {
-      txt.textContent = "(null)";
-    }
-
-    // 長さに応じてフォントサイズを調整
-    refineFontSize(
-      document.getElementById("kakugen"), 
-      j.kakugen);
-    
-    // テキストの改行を反映
-    txt.innerHTML = txt.innerText.replace(/\n/ug, "<br>");
-
-
-    // 引用元を表示
-    const ref = document.getElementById("kakugen-ref");
-    // URLぽいものだけを引用元に入れる(JSなど入れないように)
-    if (j.url && isHttpLink(j.url)) {
-      ref.href = j.url;
-      ref.hidden = false;
-    } else {
-      ref.hidden = true;
-    }
-
-    document.getElementById("kakugen").hidden = false;
-  });  
+  .then(showKakugen);
 }
 
 
 /**
- * おもしろくないリンクをはじくための関数
+ * オブジェクトからランダムに一つ採取する
+ * [a] -> a
+ * @param {Object or Array} items
+ * @returns {item}
+ */
+function pickRandomly(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+
+/**
+ * JSON を取得する
+ * @param {Response} res
+ * @returns {JSON}
+ */
+function extractJSON(res) {
+  console.log("API response: ", res); 
+  return res.json();
+}
+
+
+/**
+ * 格言を表示する
+ * 
+ * @param {{t: String, u: null|String }} j - 一格言のJSON形式
+ * @returns {JSON} - j をそのまま返す
+ */
+function showKakugen(j) {
+  console.log("JSOK(JavaScript Object Kakugen):", j);
+
+  // 格言内容を表示
+  const txt = document.getElementById("kakugen-txt");
+  if (j.t) {
+    txt.textContent = j.t;
+  } else {
+    txt.textContent = "(null)";
+  }
+
+  // フォントサイズを調整
+  refineFontSize(document.getElementById("kakugen"), j.t);
+  
+  // テキストの改行を反映
+  txt.innerHTML = txt.innerText.replace(/\n/ug, "<br>");
+
+
+  // 引用元を表示
+  const ref = document.getElementById("kakugen-ref");
+  // URLぽいものだけを引用元に入れる(JSなど入れないように)
+  if (j.u && isHttpLink(j.u)) {
+    ref.href = j.u;
+    ref.hidden = false;
+  } else {
+    ref.hidden = true;
+  }
+
+  document.getElementById("kakugen").hidden = false;
+
+  return j;
+}
+
+
+/**
+ * おもしろくないリンクをはじく
  * 
  * @param {string} url
  * @returns {boolean} 
@@ -68,7 +99,10 @@ function isHttpLink(url) {
          (u.protocol == "http:" || u.protocol == "https:");
 }
 
+
 /**
+ * フォントサイズをテキストに合わせて調整する
+ * サイズに合わせた CSS の class を設定している。
  * 
  * @param {Element} node - Target Element
  * @param {string} msg - Message
@@ -76,6 +110,7 @@ function isHttpLink(url) {
 function refineFontSize(node, msg) {
   const len = Array.of(...msg).length;
 
+  // 長さごとの CSS class を定義
   const xs = [
     {len: 130,  clsName: "small"},
     {len: 80,   clsName: "ltl-small"},
